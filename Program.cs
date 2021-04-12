8cbc5f2b4db8aa2a6350796ccb9ab0793adb729b
Mikael <mikael.sardor@studerande.plushogskolan.se>
2021-04-12 11:14:28 +0200

    HELLO!

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace magenta_popcorn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 6;
+      int b = 10;
+
+      int result = a + b;
+
+      Console.WriteLine("Hello World! " + result);
+        }
+    }
+}
